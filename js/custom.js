4dd3c617c74de00491e29c607ee9b631bad70d6d
Samuel Henriksson <sazmer@gmail.com>
2014-10-18 09:07:12 +0200

    push+weinre

--- js/custom.js
+++ js/custom.js
@@ -85,6 +85,76 @@ function onDeviceReady() {
 		link.rel = "stylesheet";
 		document.getElementsByTagName("head")[0].appendChild(link);
 	}
+	var pushNotification;
+
+	document.addEventListener("deviceready", function() {
+		pushNotification = window.plugins.pushNotification;
+
+	});
+
+	pushNotification.register(successHandler, errorHandler, {
+		"senderID" : "996348093822",
+		"ecb" : "onNotification"
+	});
+	function successHandler(result) {
+		alert('result = ' + result);
+	}
+
+	function errorHandler(error) {
+		alert('error = ' + error);
+	}
+
+	function onNotification(e) {
+		$("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+
+		switch( e.event ) {
+		case 'registered':
+			if (e.regid.length > 0) {
+				$("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+				// Your GCM push server needs to know the regID before it can push to this device
+				// here is where you might want to send it the regID for later use.
+				console.log("regID = " + e.regid);
+			}
+			break;
+
+		case 'message':
+			// if this flag is set, this notification happened while we were in the foreground.
+			// you might want to play a sound to get the user's attention, throw up a dialog, etc.
+			if (e.foreground) {
+				$("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
+
+				// on Android soundname is outside the payload.
+				// On Amazon FireOS all custom attributes are contained within payload
+				var soundfile = e.soundname || e.payload.sound;
+				// if the notification contains a soundname, play it.
+				var my_media = new Media("/android_asset/www/" + soundfile);
+				my_media.play();
+			} else {// otherwise we were launched because the user touched a notification in the notification tray.
+				if (e.coldstart) {
+					$("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+				} else {
+					$("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+				}
+			}
+
+			$("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+			//Only works for GCM
+			$("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+			//Only works on Amazon Fire OS
+			$status.append('<li>MESSAGE -> TIME: ' + e.payload.timeStamp + '</li>');
+			break;
+
+		case 'error':
+			$("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+			break;
+
+		default:
+			$("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+			break;
+		}
+	}
+
+
 	$("#klinkurtnew").hide();
 	$("#vckurtnew").hide();
 	$("#ifylltnew").hide();
@@ -100,7 +170,6 @@ function onDeviceReady() {
 	$("#splash").show();
 
 	window.deviceOS = device.platform;
-	
 
 	//    console.log(deviceOS);
 	//    if (deviceOS === "DEACTIVATEDAndroid") {
