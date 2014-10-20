b502ce1e1196ca123635783137bab7436c6fc3ff
Samuel Henriksson <sazmer@gmail.com>
2014-10-20 09:21:36 +0200

    pushwoosh added

--- js/custom.js
+++ js/custom.js
@@ -47,6 +47,35 @@ function clearKK() {
 	 });*/
 	console.log('Inskickad KK3');
 }
+function initPushwoosh() {
+	var pushNotification = window.plugins.pushNotification;
+
+	//set push notifications handler
+	document.addEventListener('push-notification', function(event) {
+		var title = event.notification.title;
+		var userData = event.notification.userdata;
+
+		if ( typeof (userData) != "undefined") {
+			console.warn('user data: ' + JSON.stringify(userData));
+		}
+
+		alert(title);
+	});
+
+	//initialize Pushwoosh with projectid: "996348093822", appid : "PUSHWOOSH_APP_ID". This will trigger all pending push notifications on start.
+	pushNotification.onDeviceReady({
+		projectid : "996348093822",
+		appid : "C25C8-A3BC7"
+	});
+
+	//register for pushes
+	pushNotification.registerDevice(function(status) {
+		var pushToken = status;
+		console.warn('push token: ' + pushToken);
+	}, function(status) {
+		console.warn(JSON.stringify(['failed to register ', status]));
+	});
+}
 
 function iOSversion() {
 	if (/iP(hone|od|ad)/.test(navigator.platform)) {
@@ -57,8 +86,7 @@ function iOSversion() {
 		return [0, 0, 0];
 	}
 }
-console.log("1");
-var pushNotification;
+
 var ver = new Array();
 ver = iOSversion();
 
@@ -67,6 +95,7 @@ function onBodyLoad() {
 	//    StatusBar.overlaysWebView(true);
 	navigator.splashscreen.show();
 document.addEventListener("deviceready", onDeviceReady, false);
+	document.addEventListener("deviceready", initPushwoosh, false);
 	$.mobile.page.prototype.options.domCache = false;
 
 	if (ver[0] >= 7) {
@@ -88,97 +117,7 @@ function onDeviceReady() {
 		link.rel = "stylesheet";
 		document.getElementsByTagName("head")[0].appendChild(link);
 	}
-	console.log("men kom igen");
-	$("#app-status-ul").append('<li>deviceready event received</li>');
-
-
-}
-
-// handle APNS notifications for iOS
-function onNotificationAPN(e) {
-	if (e.alert) {
-		$("#app-status-ul").append('<li>push-notification: ' + e.alert + '</li>');
-		// showing an alert also requires the org.apache.cordova.dialogs plugin
-		navigator.notification.alert(e.alert);
-	}
-
-	if (e.sound) {
-		// playing a sound also requires the org.apache.cordova.media plugin
-		var snd = new Media(e.sound);
-		snd.play();
-	}
-
-	if (e.badge) {
-		pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
-	}
-}
-
-// handle GCM notifications for Android
-function onNotification(e) {
-	$("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
-
-	switch( e.event ) {
-	case 'registered':
-		if (e.regid.length > 0) {
-			$("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
-			// Your GCM push server needs to know the regID before it can push to this device
-			// here is where you might want to send it the regID for later use.
-			console.log("regID = " + e.regid);
-		}
-		break;
-
-	case 'message':
-		// if this flag is set, this notification happened while we were in the foreground.
-		// you might want to play a sound to get the user's attention, throw up a dialog, etc.
-		if (e.foreground) {
-			$("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
-
-			// on Android soundname is outside the payload.
-			// On Amazon FireOS all custom attributes are contained within payload
-			var soundfile = e.soundname || e.payload.sound;
-			// if the notification contains a soundname, play it.
-			// playing a sound also requires the org.apache.cordova.media plugin
-			var my_media = new Media("/android_asset/www/" + soundfile);
-
-			my_media.play();
-		} else {// otherwise we were launched because the user touched a notification in the notification tray.
-			if (e.coldstart)
-				$("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
-			else
-				$("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
-		}
-
-		$("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
-		//android only
-		$("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
-		//amazon-fireos only
-		$("#app-status-ul").append('<li>MESSAGE -> TIMESTAMP: ' + e.payload.timeStamp + '</li>');
-		break;
-
-	case 'error':
-		$("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
-		break;
-
-	default:
-		$("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
-		break;
-	}
-}
-
-function tokenHandler(result) {
-	$("#app-status-ul").append('<li>token: ' + result + '</li>');
-	// Your iOS push server needs to know the token before it can push to this device
-	// here is where you might want to send it the token for later use.
-}
-
-function successHandler(result) {
-	$("#app-status-ul").append('<li>success:' + result + '</li>');
-}
-
-function errorHandler(error) {
-	$("#app-status-ul").append('<li>error:' + error + '</li>');
-}
-
+	
 
 $("#klinkurtnew").hide();
 $("#vckurtnew").hide();
